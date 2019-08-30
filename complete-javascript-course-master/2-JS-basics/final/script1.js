3574b5b1231643e347888f5afec6c57ea9e22807
ankit1364 <ankit1364@gmail.com>
2019-08-29 21:36:25 -0400

    Basic introduction done

--- complete-javascript-course-master/2-JS-basics/final/script1.js
+++ complete-javascript-course-master/2-JS-basics/final/script1.js
@@ -66,3 +66,245 @@ switch(job){
         console.log('Mike team won.' + mikeTeamavg);
     }
     
+    function calculateAge(birthYear){
+        return 2019-birthYear;
+    }
+
+    console.log(calculateAge(1992));
+function yearsUntilRetire(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65-age; 
+    console.log(firstName+' retires in ' + retirement +' years');
+}
+
+yearsUntilRetire(1992, 'Ankit');
+
+//function declaration
+//function whatYouDo(job, firstNam){}
+
+//function Expression
+var whatYouDo = function(job, firstName){
+    switch(job){
+        case 'teacher':
+            return firstName + ' teaches kids.';
+        case 'driver':
+            return firstName + ' drives car';
+        case 'designer':
+            return firstName + ' designes ';
+        default:
+           return firstName+ ' does nothing';
+        }
+}
+
+console.log(whatYouDo('teacher','Ankit'));
+
+var names=['Ankit','John', 'Mark'];
+
+var years= new Array(1992,1969,1948);
+
+console.log(names + years[0]);
+names[3] = 'Ben';
+names.push('Mery');
+names.shift('Smith');
+console.log(names);
+names.pop();
+console.log(names.indexOf('Ben'));
+console.log(names);
+
+
+
+function tipCalculator(ttl){
+    var percentage;
+    if(ttl<50){
+        percentage = .2
+    }
+    else if(ttl>=50&& ttl<200){
+        percentage = .15
+    }
+    else{
+        percentage = .10
+    }
+    return percentage*ttl;
+}
+var tips=[];
+var bills  = [];
+console.log(tipCalculator(100));
+bills=[124,48,268];
+tips =[tipCalculator(bills[0]),
+    tipCalculator(bills[1]),
+        tipCalculator(bills[2])];
+console.log(bills);
+console.log(tips);
+
+
+/***************************
+ * object and its properties
+ */
+
+ //object literal
+ var john = {
+     firstName : 'John',
+     lastName : 'Smith',
+     birthYear : 1990,
+     family :['Jane','Mark','Bob','Emily'],
+     job: 'Teacher',
+     isMarries : false
+ };
+console.log(john.firstName);
+console.log(john['lastName']);
+
+//new object syntax
+var jane = new Object();
+jane.firstName = 'jane';
+jane.lastName = 'Smith';
+jane.birthYear = 1990;
+
+/**
+ * object and methods
+ */
+
+var john = {
+    firstName : 'John',
+    lastName : 'Smith',
+    birthYear : 1992,
+    family :['Jane','Mark','Bob','Emily'],
+    job: 'Teacher',
+    isMarries : false,
+    calcAge: function(){
+        this.age = 2019 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log('age ' + john.age);
+
+
+var mark={
+    fullName: "Mark Smith",
+    mass:   10,
+    height: 1.8,
+    calcBMI: function(){
+        this.bmi =this.mass/(this.height * this.height); 
+    }
+};
+
+var jon={
+    fullName: "John Smith",
+    mass:   20,
+    height: 1.6,
+    calcBMI: function(){
+        this.bmi =this.mass/(this.height * this.height); 
+    }
+};
+
+var markBMI = mark.calcBMI();
+var jonBMI = jon.calcBMI();
+console.log(mark.bmi);
+console.log(jon.bmi);
+if(mark.bmi>jon.bmi){
+    console.log(mark.fullName+' has '+ mark.bmi+'. and It is higher than ' +jon.fullName);
+}
+else if(mark.bmi<jon.bmi){
+    console.log(jon.fullName+' has '+ jon.bmi+'. and It is higher than ' +mark.fullName);
+}
+else{
+    console.log('Both persons BMI is equal');
+}
+
+
+
+var bills=[];
+
+var john={
+    fullName: 'John Smith',
+    billamount:[124,40,268,180,42],
+    calcTips: function(){
+        this.tips=[];
+        this.finalbill=[];
+       for(var i=0; i<this.billamount.length;i++){
+           var percentage; 
+           var bill = this.billamount[i];
+        if(bill<50){
+            percentage = .2
+        }
+        else if(bill>=50&&bill<200){
+            percentage = .15
+        }
+        else{
+            percentage = .1
+        }
+        this.tips[i] = bill *percentage;
+        this.finalbill[i] = bill + (bill*percentage);
+       } 
+    }
+};
+
+john.calcTips();
+console.log(john);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
